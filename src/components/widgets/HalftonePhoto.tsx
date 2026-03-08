e59039f7761c129ed5bdda8134ab3739d7f618f2
GLaDO8 <gshreyas@icloud.com>
2026-03-08 16:59:09 +0530

    changed hover effect on photoframe

--- src/components/widgets/HalftonePhoto.tsx
+++ src/components/widgets/HalftonePhoto.tsx
@@ -1,56 +1,30 @@
-import { motion, useMotionValue, useSpring } from "motion/react";
-import { useSpringConfig } from "@/lib/spring-config";
+import { motion } from "motion/react";
 
 interface Props {
-  src: string;
-  alt?: string;
+	src: string;
+	alt?: string;
 }
 
 export default function HalftonePhoto({ src, alt = "Photo" }: Props) {
-  const frameConfig = useSpringConfig("photoFrameTilt");
-  const rotateX = useMotionValue(0);
-  const rotateY = useMotionValue(0);
-
-  const springX = useSpring(rotateX, frameConfig);
-  const springY = useSpring(rotateY, frameConfig);
-
-  function handlePointerMove(e: React.PointerEvent<HTMLDivElement>) {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
-    rotateY.set(((e.clientX - centerX) / (rect.width / 2)) * 4);
-    rotateX.set(((e.clientY - centerY) / (rect.height / 2)) * -4);
-  }
-
-  function handlePointerLeave() {
-    rotateX.set(0);
-    rotateY.set(0);
-  }
-
-  return (
-    <motion.div
-      className="w-[300px] h-[200px] shrink-0 relative"
-      style={{
-        rotateX: springX,
-        rotateY: springY,
-        transformPerspective: 800,
-      }}
-      onPointerMove={handlePointerMove}
-      onPointerLeave={handlePointerLeave}
-    >
-      <img
-        src={src}
-        alt={alt}
-        className="block w-full h-full -rotate-2 border-5 border-white bg-halftone-base object-cover shadow-lg rounded-[4px]"
-      />
-      <div className="absolute left-[216px] top-[127px]">
-        <img
-          src="/postit-me.svg"
-          alt="Post-it note saying Me!"
-          width={97}
-          height={105}
-        />
-      </div>
-    </motion.div>
-  );
+	return (
+		<motion.div
+			className="w-[300px] h-[200px] shrink-0 relative"
+			whileHover={{ rotate: -4 }}
+			transition={{ type: "spring", stiffness: 300, damping: 20 }}
+		>
+			<img
+				src={src}
+				alt={alt}
+				className="block w-full h-full -rotate-2 border-5 border-white bg-halftone-base object-cover shadow-lg rounded-sm"
+			/>
+			<div className="absolute left-[216px] top-[127px]">
+				<img
+					src="/postit-me.svg"
+					alt="Post-it note saying Me!"
+					width={97}
+					height={105}
+				/>
+			</div>
+		</motion.div>
+	);
 }
