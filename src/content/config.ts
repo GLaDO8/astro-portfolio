78bf9ef3a04dae99479eb657e85bdc0aaeb46a84
GLaDO8 <gshreyas@icloud.com>
2026-03-17 10:00:03 +0530

    Added drafts

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,30 +3,31 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
 const notes = defineCollection({
-	loader: glob({ pattern: "**/*.mdoc", base: "./src/content/notes" }),
-	schema: z.object({
-		title: z.string(),
-		subtitle: z.string(),
-		description: z.string().optional(),
-		date: z.date(),
-		draft: z.boolean().default(false),
-		tags: z.array(z.string()).default([]),
-	}),
+  loader: glob({ pattern: "**/*.mdoc", base: "./src/content/notes" }),
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    description: z.string().optional(),
+    date: z.date(),
+    draft: z.boolean().default(false),
+    tags: z.array(z.string()).default([]),
+  }),
 });
 
 const caseStudies = defineCollection({
-	loader: glob({
-		pattern: "**/*.mdoc",
-		base: "./src/content/case-studies",
-	}),
-	schema: z.object({
-		title: z.string(),
-		subtitle: z.string(),
-		description: z.string().optional(),
-		date: z.date(),
-		draft: z.boolean().default(false),
-		tags: z.array(z.string()).default([]),
-	}),
+  loader: glob({
+    pattern: "**/*.mdoc",
+    base: "./src/content/case-studies",
+  }),
+  schema: z.object({
+    title: z.string(),
+    subtitle: z.string(),
+    description: z.string().optional(),
+    date: z.date(),
+    draft: z.boolean().default(false),
+    tags: z.array(z.string()).default([]),
+    theme: z.string().optional(),
+  }),
 });
 
 export const collections = { notes, caseStudies };
